7e0060832a69ebd77ec0f947700dd85ea188cfb8
ewentzh <moypray@gmail.com>
2012-07-04 20:00:07 +0800

    protype for memory detectorgit st

---
+++ include/biTree.h
@@ -0,0 +1,44 @@
+/*********************************
+ *
+ *
+** */
+
+
+#ifndef __BITREE__MEM_PRJECT__H__
+#define __BITREE__MEM_PRJECT__H__
+
+struct BiNode{
+  void* elementPtr;
+  int   key;
+  BiNode* parent;
+  BiNode* lChild;
+  BiNode* rChild;
+ 
+  // BiNode Construction Function!
+  BiNode(){
+   elementPtr = NULL;
+   priority = 0;
+   parent = NULL;
+   lChild = NULL;
+   rChild = NULL;
+ } 
+};
+
+class BiTree
+{
+public:
+  void BiTree();
+  ~BiTree();
+
+  BiNode* getBiTreeRoot() const;
+  void    insertBiNode(BiNode* node);
+  void    
+private:
+  BiNode* biNodeRoot;
+};
+
+
+#endif
+
+
+
---
+++ src/memDetector.cpp
@@ -0,0 +1,39 @@
+/*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+* */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int formatLog(char* file,int line,int addr,int action)
+{
+  char buf[200] = {0};
+  sprintf(buf,"File:%s,Line:%d,has %s Addr 0x%08X\n",file,line,action==1?"free":"malloc",addr);
+  printf(buf);
+}
+
+void* debug_malloc(char* file,int line,int size)
+{
+  char buf[200] = {0} ;
+  void* ptr = NULL;
+  ptr = malloc(size);
+  formatLog(file,line,(int)ptr,0);
+  
+  return malloc(size);
+}
+
+
+void debug_free(char* file,int line,void* addr)
+{
+  formatLog(file,line,(int)addr,1);
+  free(addr);
+}
